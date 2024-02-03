0ebfbd810da1cad2a750379a31a4387fc951424f
kananilai <kana.nilai4646@gmail.com>
2024-02-03 17:52:22 +0900

    🎨 Enum型

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Max",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "Max",
     age: 30,
     hobbies: ["Sports", "Cooking"],
-    role: [2, "author"],
+    role: Role.ADMIN,
 };
 var favoriteActivities;
 favoriteActivities = ["Sports"];
@@ -10,4 +27,7 @@ console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+}
+if (person.role === Role.ADMIN) {
+    console.log("is admin");
 }
