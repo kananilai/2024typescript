27b95d7cda0caf3896d7c4a55f30969fc5a0fead
kananilai <kana.nilai4646@gmail.com>
2024-02-03 17:26:41 +0900

    🎨 object型

--- app.ts
+++ app.ts
@@ -1,15 +1,6 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const resutl = n1 + n2;
-  if (showResult) {
-    console.log(phrase + resutl);
-  } else {
-    return result;
-  }
-}
+const person = {
+  name: "Max",
+  age: 30,
+};
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = "Result: ";
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person);
