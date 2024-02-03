c4e55ccaf24b6f1d3112751af6b61fa048b7ec09
kananilai <kana.nilai4646@gmail.com>
2024-02-03 17:20:22 +0900

    🎨 string,boolean型

--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
-function add(n1:number, n2:number) {
-  return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const resutl = n1 + n2;
+  if (showResult) {
+    console.log(phrase + resutl);
+  } else {
+    return result;
+  }
 }
 
-const number1:number = 5;
-const number2:number = 2.8;
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result: ";
 
-const result:number = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
