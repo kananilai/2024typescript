0a03b40b7c89ebf9670662570fb8c41b5258be8c
kananilai <kana.nilai4646@gmail.com>
2024-02-03 18:31:06 +0900

    🎨 function型とコールバック

--- app.js
+++ app.js
@@ -4,4 +4,14 @@ function add(n1, n2) {
 function printResult2(num) {
     console.log("Result: " + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
 printResult2(add(5, 12));
--- app.ts
+++ app.ts
@@ -6,4 +6,18 @@ function printResult2(num: number): void {
   console.log("Result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
+
+let combineValues: (a: number, b: number) => number;
+combineValues = add;
+
+console.log(combineValues(8, 8));
+
 printResult2(add(5, 12));
