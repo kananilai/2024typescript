8fcde5e7d83a2316bbc02d0313f22769f7b464ab
kananilai <kana.nilai4646@gmail.com>
2024-02-04 11:13:58 +0900

    🎨 class

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+}
+
+const accounting = new Department("Accounting");
+console.log(accounting);
+accounting.describe();
+
+const accountingCopy = {
+  name: "DUMMY",
+  describe: accounting.describe,
+};
+
+accountingCopy.describe();
